bddb208e3dcba4f9f55cf2edca9adc5a2e554fda
Cary Carter <ccarterdev@gmail.com>
2023-01-21 12:26:15 -0600

    sleepy

--- fedrit/fedritapp/models.py
+++ fedrit/fedritapp/models.py
@@ -4,22 +4,31 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 
 
-class GlobalPlatform(models.Model):
-    domain = models.CharField(max_length=255, unique=True)
-    name = models.CharField(max_length=255, unique=True)
-    fingerprint = models.CharField(max_length=255, unique=True)
+class GPGKey(models.Model):
+    fingerprint = models.CharField(max_length=255, unique=True, primary_key=True)
     pubkey = models.TextField(unique=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+
+class GlobalPlatform(models.Model):
+    domain = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255, unique=True)
+    gpgkey = models.ForeignKey(GPGKey, null=True, blank=True, on_delete=models.DO_NOTHING)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
 class GlobalUser(models.Model):
     id = models.UUIDField(primary_key=True)
     origin_platform = models.OneToOneField(GlobalPlatform)
 
+
 class PlatformUser(models.Model):
     user = models.OneToOneField(User, blank=True, null=True, on_delete=models.CASCADE)
-    platform = models.OneToOneField(Platform, on_delete=models.CASCADE)
+    platform = models.OneToOneField(GlobalPlatform, on_delete=models.CASCADE)
 
 
 class Community(models.Model):
