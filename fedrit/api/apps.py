5ab2aaf245d1a8dfa11b7afe21621701dca5fafa
Cary Carter <ccarterdev@gmail.com>
2023-01-27 20:53:55 -0600

    fedritapp to api

--- fedrit/api/apps.py
+++ fedrit/api/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FedritappConfig(AppConfig):
+class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fedritapp'
+    name = 'api'
--- fedrit/api/models.py
+++ fedrit/api/models.py
@@ -5,7 +5,8 @@ from django.utils import timezone
 
 
 class PGPKey(models.Model):
-    fingerprint = models.CharField(max_length=255, unique=True, primary_key=True)
+    fingerprint = models.CharField(
+        max_length=255, unique=True, primary_key=True)
     pubkey = models.TextField(unique=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
@@ -29,23 +30,31 @@ class GlobalPlatform(models.Model):
 
 class GlobalUser(models.Model):
     uuid = models.UUIDField(primary_key=True)
-    origin_platform = models.OneToOneField(GlobalPlatform)
+
+    # original platform attributes
+    origin_platform = models.OneToOneField(
+        GlobalPlatform, on_delete=models.DO_NOTHING)
     origin_username = models.CharField(max_length=255)
-    pgpkey = models.ForeignKey(PGPKey, null=True, blank=True, on_delete=models.DO_NOTHING)
-    user = models.OneToOneField(User, blank=True, null=True, on_delete=models.DO_NOTHING)
+
+    pgpkey = models.ForeignKey(
+        PGPKey, null=True, blank=True, on_delete=models.DO_NOTHING)
+    user = models.OneToOneField(
+        User, blank=True, null=True, on_delete=models.DO_NOTHING)
 
     def __repr__(self):
         return f'<GlobalUser {self.id}>'
 
 
 class Community(models.Model):
+    uuid = models.UUIDField(primary_key=True)
     COMMUNITY_TYPES = (
         ('SUB', 'Subreddit'),
         ('IMGBOARD', 'ImageBoard'),
         ('FORUM', 'Forum'),
     )
 
-    community_type = models.CharField(max_length=50, choices=COMMUNITY_TYPES, default='SUB')
+    community_type = models.CharField(
+        max_length=50, choices=COMMUNITY_TYPES, default='SUB')
     platform = models.OneToOneField(GlobalPlatform, on_delete=models.CASCADE)
     name = models.CharField(max_length=50, unique=True)
 
@@ -54,10 +63,19 @@ class Community(models.Model):
 
     def __repr__(self):
         return f'<Community {self.name} type={self.get_community_type.display()}>'
-    
+
 
 class CommunityPost(models.Model):
-    author = models.OneToOneField()
+    uuid = models.UUIDField(primary_key=True)
+
+    # post attributes
+    author = models.OneToOneField(
+        GlobalUser, blank=True, null=True, on_delete=models.DO_NOTHING)
+    text = models.TextField(blank=True, default='')
+    url = models.CharField(max_length=255, blank=True, default='')
+    title = models.CharField(max_length=255)
+    platform = models.OneToOneField(
+        GlobalPlatform, on_delete=models.DO_NOTHING)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
