444f970ec72bd3d36332bcfebf73624ec7d02f5c
Cary Carter <ccarterdev@gmail.com>
2023-02-07 18:42:24 -0600

    react lol

--- frontend/src/AuthProvider.tsx
+++ frontend/src/AuthProvider.tsx
@@ -12,43 +12,30 @@ export interface AuthContextProps {
 
 const AuthProvider = (props: any) => {
   const [user, setUser] = useState<PlatformUser | null>(null);
-  const [isLoading, setLoading] = useState<boolean>(false);
   const [getUser, setGetUser] = useState<boolean | null>(null);
-  //const [token, setToken] = useState<string | null | boolean>(false);
   const token = localStorage.getItem('token');
-
 
   const fetchUser =  async(token: string) => {
     const response =  await axios.post(`${API_URL}/tokenuser`, {
       'token': token,
     }).then((response) => {
-      console.log('then fetchuser');
       setUser(response.data);
     }).catch((err) => {
       console.error(err);
     }).finally(() => {
-      console.log('finalyl fetchuser');
     });
   };
 
-  console.log('outside useffect');
-
   useEffect(() => {
-    console.log(`inside useffect setloading ${isLoading} ${token} ${user}`);
     if (token && getUser === null) {
       setGetUser(true);
     } else if (token && getUser === true) {
-      setGetUser(false);
-      setLoading(true)
-      console.log('storedtoken and user calling fetchuser');
-      fetchUser(token).then(() => setLoading(false));
+      fetchUser(token).then(() => setGetUser(false))
     }
   }, [getUser]);
 
-  console.log('before return');
-  
   return (
-    <AuthContext.Provider value={{ user, setUser, isLoading }}>
+    <AuthContext.Provider value={{ user, setUser,  }}>
       {props.children}
     </AuthContext.Provider>
   );
